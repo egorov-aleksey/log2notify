8292c63ab0629853dddf4548c777566e177db1a4
meatbot <egorov.lexer@yandex.ru>
2017-07-09 11:27:17 +0700

    Setup

---
+++ log2notify/__init__.py
@@ -0,0 +1,93 @@
+import argparse
+import os
+
+import pyinotify as i
+from plyer import notification
+
+
+class Watcher(i.ProcessEvent):
+    pointers = {}
+
+    def __init__(self, path):
+        if os.path.isfile(path):
+            self.register_file(path)
+        elif os.path.isdir(path):
+            self.register_dir(path)
+
+    def register_dir(self, dir):
+        for item in os.listdir(dir):
+            path = os.path.join(dir, item)
+
+            if os.path.isfile(path):
+                self.register_file(path)
+            elif os.path.isdir(path):
+                self.register_dir(path)
+
+    def register_file(self, file):
+        print(file)
+        self.pointers[file] = os.path.getsize(file)
+
+    def register_new_file(self, file):
+        self.pointers[file] = 0
+
+    def unregister_file(self, file):
+        self.pointers.pop(file)
+
+    def update_file_pointer(self, file):
+        self.pointers[file] = os.path.getsize(file)
+
+    def read_file(self, file):
+        pointer = self.pointers[file]
+        f = open(file)
+        f.seek(pointer)
+        res = f.read()
+        f.close()
+
+        self.update_file_pointer(file)
+
+        return res
+
+    def process_IN_MODIFY(self, event):
+        print(event)
+
+        file_name = event.pathname
+
+        if os.path.isfile(file_name):
+            if file_name not in self.pointers:
+                self.register_new_file(file_name)
+
+            txt = self.read_file(file_name)
+
+            if txt:
+                try:
+                    notification.notify(
+                        title=file_name,
+                        message=txt
+                    )
+                except Exception as e:
+                    notification.notify(
+                        title="Exception when notify: %s" % str(file_name),
+                        message=str(e)
+                    )
+
+    def process_IN_DELETE(self, event):
+        print(event)
+
+        file_name = event.pathname
+
+        if os.path.isfile(file_name):
+            self.unregister_file(file_name)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", help="file or directory for watching", nargs="*", default=[os.getcwd()])
+
+    args = parser.parse_args()
+
+    wm = i.WatchManager()
+    for p in args.path:
+        wm.add_watch(p, i.ALL_EVENTS, proc_fun=Watcher(p))
+
+    notifier = i.Notifier(wm)
+    notifier.loop()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='log2notify',
+      version='0.1',
+      description='Provides file changes to kde tray notification',
+      long_description=readme(),
+      url='http://github.com/meatbot/log2notify',
+      author='Aleksei Egorov',
+      author_email='egorov.lexer@yandex.ru',
+      license='MIT',
+      packages=['log2notify'],
+      install_requires=[
+          'pyinotify',
+          'plyer',
+      ],
+      entry_points={
+          'console_scripts': ['log2notify=log2notify:main'],
+      },
+      include_package_data=True,
+      zip_safe=False)
